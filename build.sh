9ace40551fe5fd9861d34f3b3a41b94a78783e18
Mark Tomm <mark.tomm@martem.ee>
2018-09-15 19:03:31 +0300

    script changes

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gcc --std=c99 server.c open62541/build/open62541.c -o server
-gcc --std=c99 client.c open62541/build/open62541.c -o client
+gcc --std=c99 -o server -I ./open62541/build server.c open62541/build/open62541.c 
+gcc --std=c99 -o client -I ./open62541/build client.c open62541/build/open62541.c 
--- link-build.sh
+++ link-build.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
-gcc --std=c99 -o server server.c -I . -L ./open62541/build/bin/ -lopen62541
-gcc --std=c99 -o client client.c -I . -L ./open62541/build/bin/ -lopen62541
+PWD=$(pwd)
+LIB_DIR=${PWD}/open62541
+BUILD_DIR=${LIB_DIR}/build
+
+cd ${LIB_DIR}
+sudo rm -r ${BUILD_DIR}
+mkdir ${BUILD_DIR}
+cd ${BUILD_DIR}
+
+cmake -DBUILD_SHARED_LIBS=ON \
+-DUA_ENABLE_AMALGAMATION=ON \
+-DUA_ENABLE_FULL_NS0=ON \
+-DUA_LOGLEVEL=100 \
+-DUA_DEBUG=ON \
+-DUA_DEBUG_DUMP_PKGS=ON ..
+
+make -j9
+sudo make install
+
+cd ${PWD}
+gcc --std=c99 -o server server.c -I ./open62541/build -L ./open62541/build/bin/ -lopen62541
+gcc --std=c99 -o client client.c -I ./open62541/build -L ./open62541/build/bin/ -lopen62541
+
+./server
